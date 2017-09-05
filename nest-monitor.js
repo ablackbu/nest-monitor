ceea3bac8aadd4e276a9fddac94fc1af4c7ad6fa
Andrew Blackburn <andrew.blackburn@protonmail.com>
2017-09-05 19:18:50 -0400

    Linting cleanup

--- nest-monitor.js
+++ nest-monitor.js
@@ -1,16 +1,19 @@
+'use strict';
+
 //Dependencies
+var logger = require('logger').createLogger();
 const common = require('./server/common');
 const nest = require('./server/nest');
 
-var interval = 5*60*1000; //In ms, default to 5 minutes
+var interval = 5 * 60 * 1000; //In ms, default to 5 minutes
 
 const iteration = () => {
-    common.getConfig().then((config) => {
-        interval = config.nestCheckFrequency*60*1000;
-        nest.fetchStatus(config);
-    }).catch((err) => {
-        console.log("Error caught in iteration. ", err);
-    }) ;    
+  common.getConfig().then((config) => {
+    interval = config.nestCheckFrequency * 60 * 1000;
+    nest.fetchStatus(config);
+  }).catch((err) => {
+    logger.error('Error caught in iteration. ', err);
+  });
 };
 
 //Run once manually and kick off the nest iterations. 
--- server/nest.js
+++ server/nest.js
@@ -1,35 +1,38 @@
+'use strict';
+
+var logger = require('logger').createLogger();
 const Promise = require('bluebird');
 const nest = Promise.promisifyAll(require('unofficial-nest-api'));
 const common = require('./common');
 
-module.exports.fetchStatus  = (config) => {
-    login(config).then((data) => {
-        //can't use promisify for this function; 
-        //it doesn't satisify the contract required by bluebird
-        nest.fetchStatus((data) => {
-            calculateAndSetFanStatus(config, data);
-        });
+module.exports.fetchStatus = (config) => {
+  login(config).then((data) => {
+    //can't use promisify for this function; 
+    //it doesn't satisify the contract required by bluebird
+    nest.fetchStatus((data) => {
+      calculateAndSetFanStatus(config, data);
     });
+  });
 };
 
 const login = (config) => {
-    return nest.loginAsync(config.username, config.password);
+  return nest.loginAsync(config.username, config.password);
 };
 
 const calculateAndSetFanStatus = (config, data) => {
-    common.getSensorTemperature(config.sensorFile).then((sensorTemp) => {
-        for (var deviceId in data.device) {
-            if (data.device.hasOwnProperty(deviceId)) {
-                var device = data.shared[deviceId];
-                common.calculateFanStatus(device, sensorTemp) ? nest.setFanModeOn() : 
-                    nest.setFanModeAuto();
-                return;
-            }
-        }
-    }).catch((err) => {
-        console.log("Error caught in calculateAndSetFanStatus. ", err);        
-    });
-    
-    
+  common.getSensorTemperature(config.sensorFile).then((sensorTemp) => {
+    for (var deviceId in data.device) {
+      if (data.device.hasOwnProperty(deviceId)) {
+        var device = data.shared[deviceId];
+        common.calculateFanStatus(device, sensorTemp) ? nest.setFanModeOn() :
+          nest.setFanModeAuto();
+        return;
+      }
+    }
+  }).catch((err) => {
+    logger.error('Error caught in calculateAndSetFanStatus. ', err);
+  });
+
+
 };
-   
+
